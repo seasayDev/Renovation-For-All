dbcc3d66837adb686c0893dc8da50f5a58744d71
seasayDev <saliouseasay@gmail.com>
2022-07-16 11:58:01 -0400

    Finalize the user menu of the program

--- basic-version/main.cpp
+++ basic-version/main.cpp
@@ -14,8 +14,8 @@
 
 #define LINE  "---------------------------------------------------------------------------------------" 
 #define WELCOME  " Welcome to the Renovation-For-All billing system."
-#define DESCRIPTION  " This program makes it possible to calculate the invoice\n for the installation of a floor in the given"\
-	             "surface according to the price per square foot and the type of floor"  
+#define DESCRIPTION  " This program makes it possible to calculate the invoice for the installation of a floor\n in the given"\
+	             "surface according to the price per square foot and the type of floor."  
 #define MENU "*** Menu of choice ***"\
              "\n1.  Invoice the installation of a floor"\
              "\n2.  Display the number of floors installed and the total amount of all installations"\
