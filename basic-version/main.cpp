a1fbb7cfc05e95c9f1c7c69de76f0a09aed8d286
seasayDev <saliouseasay@gmail.com>
2022-07-16 11:41:23 -0400

    Add the Menu & Description of the software

--- basic-version/main.cpp
+++ basic-version/main.cpp
@@ -10,10 +10,22 @@
  */ 
 
 #include <iostream>
+
+
+#define LINE  "---------------------------------------------------------------------------------------" 
+#define WELCOME  "Welcome to the Renovation-For-All billing system."
+#define DESCRIPTION  "This program makes it possible to calculate the invoice\nfor the installation of a floor in the given"\
+	             "surface according to the price per square foot and the type of floor"  
+#define MENU "*** Menu of choice ***"\
+             "\n1.  Invoice the installation of a floor"\
+             "\n2.  Display the number of floors installed and the total amount of all installations"\
+	     "\n3.  Quit the program" 
+
+
 using namespace std; 
 
 int main() {
-
+ cout<<MENU;  
 
 	return 0; 
 }
