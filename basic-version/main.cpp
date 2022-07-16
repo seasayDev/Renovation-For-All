15bd2fe058b4ccc534f7f866925e0934c1ebf630
seasayDev <saliouseasay@gmail.com>
2022-07-16 11:12:57 -0400

    Add the description of the project

--- basic-version/main.cpp
+++ basic-version/main.cpp
@@ -1,3 +1,14 @@
+/**
+ * R-F-A is a simple software that allows you to place orders
+ * for the installation of different floor, then automate 
+ * order taking to facilitate the work of clerks and then 
+ * provide activity statistics for owners. 
+ *
+ * @author Saliou.
+ *
+ * @version 2022-07-16. 
+ */ 
+
 #include <iostream>
 using namespace std; 
 
