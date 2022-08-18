17b5c1a4fa4c543e706b3811e648acdfc987afce
seasayDev <saliouseasay@gmail.com>
2022-08-18 06:37:55 -0400

    Write the invoice prompt code

--- basic-version/main.cpp
+++ basic-version/main.cpp
@@ -21,12 +21,12 @@
              "\n2.  Display the number of floors installed and the total amount of all installations"\
 	     "\n3.  Quit the program"\
 	     "\n\nEnter your choice : "  
-#define MSG_SOLL_F_NAME "Enter customer`s last name : "
-#define MSG_SOLL_L_NAME "Enter customer`s first name : "
+#define MSG_SOLL_F_NAME "Enter customer`s first name : "
+#define MSG_SOLL_L_NAME "Enter customer`s last name : "
 #define MSG_SOLL_PHONE "Enter customer`s phone number : "
 #define MSG_SOLL_ADDR "Enter customer`s address : "
-#define MSG_SOLL_FLOOR "Enter the floor type (1 for ceramic, 2 for the vinyl tiles, "\
-                       "3 for linoleum, 4 for hardwood, 5 for floating floor) : "
+#define MSG_SOLL_FLOOR "Enter the floor type (1 for ceramic, \n2 for the vinyl tiles, "\
+                       "3 for linoleum, \n4 for hardwood, 5 for floating floor) : "
 
 #define MSG_SOLL_SURFACE "Enter the area to be covered in square feet (greather than 0) : "
 #define MSG_SOLL_BUILD "Enter building type (R or r for residential, and E or e for company) : "
@@ -48,9 +48,16 @@ int main() {
 
 	//program variables 
 	int choice; 
-        bool quit;
+    bool quit;
 	bool validation; 
-
+	string lname;
+    string fname;
+	string phone;
+	string address;
+	int floor;
+	int surface;
+	char building;
+	char payment;
 
 	//Initializing variables 
 	quit = false;
@@ -86,6 +93,38 @@ int main() {
 		switch(choice){ 
 			case ONE:
 
+			//Enter the customer last-name 
+			cout<<NL<<NL<<MSG_SOLL_L_NAME;
+            cin>>lname;   	
+
+			//Enter the customer first-name 
+			cout<<NL<<MSG_SOLL_F_NAME;
+            cin>>fname; 
+
+			//Enter the customer phone number
+			cout<<NL<<MSG_SOLL_PHONE; 
+			cin>>phone; 
+
+			//Enter the customer address 
+			cout<<NL<<MSG_SOLL_ADDR;
+			cin>>address;
+
+			//Enter the floor type 
+			cout<<NL<<MSG_SOLL_FLOOR;
+            cin>>floor;
+
+			//Enter the surface
+			cout<<NL<<MSG_SOLL_SURFACE;
+			cin>>surface;
+
+			//Enter the building 
+			cout<<NL<<MSG_SOLL_BUILD;
+			cin>>building;
+
+			//Enter the payment method 
+			cout<<NL<<MSG_SOLL_PAY;
+			cin>>payment; 
+			
 			break;
 			case TWO:
 			break; 
